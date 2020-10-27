d4eebbc1d3e82ea00571f35bf7751f0e5518af91
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-27 20:24:12 +0100

    Fixed a couple of prototypes

--- holberton.h
+++ holberton.h
@@ -5,29 +5,19 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdlib.h>
-
-/* String operations */
 int _strlen(char *s);
 char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_tolower(char *string);
 int alpha(int r);
-
-/* Math operations */
 int _abs(int num);
 int itoa(int n, char s[]);
 uint uitoa(uint n, char s[]);
-
-/* Conversion operations */
 int base_op(char *buffer, unsigned long n, int b, int p, int l, int *retval);
 int addstring(char *string, void *add, int pos, int *retval);
 int addnum(char *string, void *num, int pos, int *retval);
 int addunum(char *string, void *num, int pos, int *retval);
-
-/* Memory operations */
 char *_strdup(char *str);
-
-/* Print operations */
 int _putchar(char c, int *retval);
 int _print(char *string, int *retval);
 int _printf(const char *format, ...);
@@ -52,13 +42,8 @@ int addchar(char *string, void *add, int pos, int *retval);
 int addpercent(char *string, void *add, int pos, int *retval);
 int addthis(char *string, char add, int pos, int *retval);
 int address(char *string, void *add, int pos, int *retval);
-
-/* Switch Hub */
 int switch_hub(const char *f, char *b, int i, int j, va_list args, int *r);
 int spec_add(char *speclist, int speci, char c);
-
-/* Special operations */
 int rot13(char *string, void *add, int pos, int *retval);
 int rev(char *string, void *add, int pos, int *retval);
-
 #endif
