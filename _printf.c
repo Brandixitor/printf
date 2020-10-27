20dad40f0d7296c70231f15e0c4738dcb5e031c0
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-27 21:08:18 +0100

    A new printf version, removing the old one

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* _printf - prints.
+* @format: pointer.
+* Return: ..
+*/
+
+int _printf(const char *format, ...)
+{
+	unsigned int count = 0, i = 0;
+	int (*f)(va_list);
+	va_list list;
+
+	if (format == '\0')
+		return (-1);
+	va_start(list, format);
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		else if (format[i] == '%' && format[i + 1] == '\0')
+			return (-1);
+		else if (format[i] == '\0')
+			return (count);
+		else if (format[i] == '%')
+		{
+			f = getspecifier(format, i + 1);
+			i += 1;
+			if (f == NULL)
+			{
+				count += strange(format, i);
+			}
+			else
+			{
+				count = count + f(list);
+				if (format[i] == '+' || format[i] == ' ' || format[i] == '#')
+					i++;
+			}
+		}
+		i++;
+	}
+	va_end(list);
+	return (count);
+}
