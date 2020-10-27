3a57bfd804e48bf4fd434759c39378fab2340159
Brandixitor <med.salah.bourouba@gmail.com>
2020-10-27 21:16:17 +0100

    Fixed prototypes

--- holberton.h
+++ holberton.h
@@ -1,49 +1,35 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
-#include <stdlib.h>
-int _strlen(char *s);
-char *_strncpy(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-char *_tolower(char *string);
-int alpha(int r);
-int _abs(int num);
-int itoa(int n, char s[]);
-int uitoa(uint n, char s[]);
-int base_op(char *buffer, unsigned long n, int b, int p, int l, int *retval);
-int addstring(char *string, void *add, int pos, int *retval);
-int addnum(char *string, void *num, int pos, int *retval);
-int addunum(char *string, void *num, int pos, int *retval);
-char *_strdup(char *str);
-int _putchar(char c, int *retval);
-int _print(char *string, int *retval);
-int _printf(const char *format, ...);
-
 /**
- * struct op - structure to store operations
- * @op: operation input
- * @f: operation value as a function pointer
+ * struct mystr - pointer to function with corresponding letter
+ * @letter: specifier
+ * @func: print argument
  */
-typedef struct op
+typedef struct mystr
 {
-	char op;
-	int (*f)(char *string, void *add, int pos, int *retval);
-} op_t;
-op_t *initializeops(void);
-int (*get_op_func(char s))(char *, void *, int, int *);
-int binary(char *buffer, void *number, int pos, int *retval);
-int octal(char *buffer, void *number, int pos, int *retval);
-int upperhex(char *buffer, void *number, int pos, int *retval);
-int lowerhex(char *buffer, void *number, int pos, int *retval);
-int addchar(char *string, void *add, int pos, int *retval);
-int addpercent(char *string, void *add, int pos, int *retval);
-int addthis(char *string, char add, int pos, int *retval);
-int address(char *string, void *add, int pos, int *retval);
-int switch_hub(const char *f, char *b, int i, int j, va_list args, int *r);
-int spec_add(char *speclist, int speci, char c);
-int rot13(char *string, void *add, int pos, int *retval);
-int rev(char *string, void *add, int pos, int *retval);
+	char *letter;
+	int (*func)(va_list);
+} mystr;
+int _putchar(char c);
+int _printf(const char *format, ...);
+int(*getspecifier(const char *format, int i))(va_list);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_S(va_list S);
+int print_rs(va_list rs);
+int print_rot(va_list ro);
+int _print_i(va_list vi);
+int _print_b(va_list b);
+int strange(const char *format, int i);
+int _plus_i(va_list);
+int _space_i(va_list);
+int _diez_o(va_list);
+int _diez_x(va_list);
+int _diez_X(va_list);
+int _print_u(va_list);
+int _print_o(va_list);
+int _print_x(va_list);
+int _print_X(va_list);
+int _print_p(va_list);
 #endif
